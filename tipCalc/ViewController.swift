ce2d41fb1397c2590e2c3239ae7c89d4e69c77b8
Akash Gheewala <akashxg@gmail.com>
2018-10-25 14:33:10 -0400

    Initial Commit

---
+++ tipCalc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tipCalc
+//
+//  Created by Akash Gheewala on 10/25/18.
+//  Copyright © 2018 Akash Gheewala. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
